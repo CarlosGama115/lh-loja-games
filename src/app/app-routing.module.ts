22b5ba4377e520a6e04cce5395ac695961cc0c49
Carlos A Gama <95866417+CarlosGama115@users.noreply.github.com>
2023-07-14 21:33:43 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [ {path:'inicio', component: InicioComponent},
+{path:'login', component: LoginComponent}, 
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
